96e9db7a480eed65a0993921ad856dfcef81b725
mishrall <156556174+mishrall@users.noreply.github.com>
2025-03-20 22:00:57 -0400

    Update streamlit_app.py
    Added a section to display nutritional information

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,11 @@ if ingredients_list:
     #st.stop()
     
     time_to_insert = st.button('Submit Order')
-    
+
+#New section to display SmoothieFroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+ name_on_order, icon="✅")
